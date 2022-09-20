0d3f05a2645aa7d0747a6712164fe314ddf70ed5
jamshid <jamshidshamshod0705@gmail.com>
2022-09-20 21:45:42 +0500

    .

--- utility/catchError.js
+++ utility/catchError.js
@@ -1,9 +1,11 @@
-const appError = require("./appError")
+const appError = require('./appError');
 
-const catchError=(func)=>{
-  return (req,res,next,Model,options,options2)=>{
-    func(req,res,next,Model,options,options2).catch(err=>next(new appError(err.message,404)))
-  }
-}
+const catchError = (func) => {
+  return (req, res, next, Model, options, options2) => {
+    func(req, res, next, Model, options, options2).catch((err) =>
+      next(new appError(err.message, 404))
+    );
+  };
+};
 
-module.exports=catchError
+module.exports = catchError;
